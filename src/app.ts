7e0062477cd75c6a05a946add1bdef05bea62902
Istiyak Ahmed <istiyak.anonto@gmail.com>
2023-07-23 23:58:28 +0600

    setup lint,prettier,huskey,lint-staged Done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
- import  express,{Application,Request,Response} from  'express'
- import cors from "cors"
-const app:Application = express()
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 const port = 5000
-app.use (cors())
+app.use(cors())
 //parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
- 
+app.use(express.urlencoded({ extended: true }))
+
 //Testing purpose
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully')
 })
 
