fa35e1a9e26df8fa7e6c4cd1e9027eb1d391a58a
Istiyak Ahmed <istiyak.anonto@gmail.com>
2023-07-24 00:01:22 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 5000
+
 app.use(cors())
 //parser
 app.use(express.json())
